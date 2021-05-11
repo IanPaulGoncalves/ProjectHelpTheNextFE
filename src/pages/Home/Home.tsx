0ce5e2e857ae7b251bf9638f7afea32d10dc6ef0
IanPaulGoncalves <63078125+IanPaulGoncalves@users.noreply.github.com>
2021-05-10 22:24:41 -0300

    import project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+import { Box, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import Feed from '../../components/Feed/Feed';
+import Header from '../../components/Header/Header';
+import NavBar from '../../components/NavBar/NavBar';
+import '../../components/Styles/style.css';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  main: {
+    height: '100vh',
+    padding: 20
+  },
+  toolbar: {
+    minHeight: 64
+  }
+});
+
+
+function Home() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Header />
+      <div className={classes.toolbar} />
+      <main className={classes.main}>
+        <Container maxWidth="lg">
+          <Box display="flex">
+            <NavBar />
+            <Feed />
+          </Box>
+        </Container>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
