b39b9a3d9541cfe0261744a490ed877b613ad8da
IanPaulGoncalves <63078125+IanPaulGoncalves@users.noreply.github.com>
2021-05-24 00:31:10 -0300

    modal and tests

--- src/pages/Home/components/Header/Header.tsx
+++ src/pages/Home/components/Header/Header.tsx
@@ -15,8 +15,12 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import CreateIcon from '@material-ui/icons/Create';
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
 import { useAppSelector } from '../../../../app/hooks';
 import { resetLogin } from '../../../../services/authService';
+import Modal from '../../../../components/Modal/Modal';
 
 interface Props {
   // eslint-disable-next-line react/require-default-props
@@ -44,7 +48,7 @@ const useStyles = makeStyles({
     padding: 0,
     display: 'flex',
     alignItems: 'center',
-    width: '30%'
+    height: 40
   },
   input: {
     marginLeft: 10,
@@ -65,6 +69,14 @@ function Header() {
   const openNotification = Boolean(anchorNotification);
   const [anchorAccount, setAnchorAccount] = React.useState<null | HTMLElement>(null);
   const openAccount = Boolean(anchorAccount);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const account = useAppSelector(state => state.account);
   const authenticated = Boolean(account.user);
@@ -98,109 +110,160 @@ function Header() {
     <HideOnScroll {...props}>
       <AppBar color="inherit" style={{ background: '#2c387e' }}>
         <Toolbar className={classes.root}>
-          <div style={{ color: '#fff', width: '30%' }}>
-            <span>Barra de notificação</span>
+          <div style={{
+            display: 'flex', width: '100%', justifyContent: 'space-between', flexWrap: 'wrap'
+          }}
+          >
+            <div style={{ display: 'flex', alignItems: 'center', color: '#fff' }}>
+              <span>Barra de notificação</span>
+            </div>
+            <div style={{
+              display: 'flex', alignItems: 'center'
+            }}
+            >
+              <Paper component="form" className={classes.containerSearch}>
+                <InputBase
+                  className={classes.input}
+                  placeholder="Pesquise por algo"
+                  inputProps={{ 'aria-label': 'search google maps' }}
+                />
+                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+            </div>
+            {authenticated ? (
+              <div style={{ display: 'flex' }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    color="default"
+                    className={classes.button}
+                    startIcon={<CreateIcon style={{ color: 'rgb(44, 56, 126)' }} />}
+                    style={{ background: '#fff' }}
+                    onClick={handleClickOpen}
+                  >
+                    <span style={{ color: 'rgba(0, 0, 0, 0.54)' }}>Pedir ajuda</span>
+                  </Button>
+                </div>
+                <div>
+                  <IconButton
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                    aria-controls="menu-notifications"
+                    aria-haspopup="true"
+                    onClick={handleMenuNotification}
+                  >
+                    <Badge badgeContent={17} color="secondary">
+                      <NotificationsIcon style={{ color: '#fff' }} />
+                    </Badge>
+                  </IconButton>
+                  <Menu
+                    key={1}
+                    id="menu-notifications"
+                    anchorEl={anchorNotification}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    open={openNotification}
+                    onClose={handleCloseNotification}
+                  >
+                    <div>
+                      <span style={{ margin: '0px 10px' }}>
+                        Aqui estão suas notificações
+                      </span>
+                    </div>
+                  </Menu>
+                </div>
+                <div>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-account"
+                    aria-haspopup="true"
+                    onClick={handleMenuAccount}
+                    color="inherit"
+                  >
+                    <AccountCircle style={{ color: '#fff' }} />
+                  </IconButton>
+                  <Menu
+                    key={2}
+                    id="menu-account"
+                    anchorEl={anchorAccount}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    open={openAccount}
+                    onClose={handleCloseAccount}
+                  >
+                    <MenuItem onClick={handleCloseAccount}>Profile</MenuItem>
+                    <MenuItem onClick={handleCloseAccount}>My account</MenuItem>
+                    <MenuItem onClick={logout}>Sair</MenuItem>
+                  </Menu>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <Button
+                  variant="contained"
+                  color="default"
+                  className={classes.button}
+                  startIcon={<AccountCircle style={{ color: 'rgb(44, 56, 126)' }} />}
+                  onClick={login}
+                  style={{ background: '#fff' }}
+                >
+                  <span style={{ color: 'rgba(0, 0, 0, 0.54)' }}>Entrar</span>
+                </Button>
+              </div>
+            )}
           </div>
-          <Paper component="form" className={classes.containerSearch}>
-            <InputBase
-              className={classes.input}
-              placeholder="Pesquise por algo"
-              inputProps={{ 'aria-label': 'search google maps' }}
-            />
-            <IconButton type="submit" className={classes.iconButton} aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </Paper>
-          {authenticated ? (
-            <div style={{ display: 'flex', justifyContent: 'flex-end', width: '30%' }}>
-              <div>
-                <IconButton
-                  aria-label="show 17 new notifications"
-                  color="inherit"
-                  aria-controls="menu-notifications"
-                  aria-haspopup="true"
-                  onClick={handleMenuNotification}
-                >
-                  <Badge badgeContent={17} color="secondary">
-                    <NotificationsIcon style={{ color: '#fff' }} />
-                  </Badge>
-                </IconButton>
-                <Menu
-                  key={1}
-                  id="menu-notifications"
-                  anchorEl={anchorNotification}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  open={openNotification}
-                  onClose={handleCloseNotification}
-                >
-                  <div>
-                    <span style={{ margin: '0px 10px' }}>
-                      Aqui estão suas notificações
-                    </span>
-                  </div>
-                </Menu>
-              </div>
-              <div>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-account"
-                  aria-haspopup="true"
-                  onClick={handleMenuAccount}
-                  color="inherit"
-                >
-                  <AccountCircle style={{ color: '#fff' }} />
-                </IconButton>
-                <Menu
-                  key={2}
-                  id="menu-account"
-                  anchorEl={anchorAccount}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  open={openAccount}
-                  onClose={handleCloseAccount}
-                >
-                  <MenuItem onClick={handleCloseAccount}>Profile</MenuItem>
-                  <MenuItem onClick={handleCloseAccount}>My account</MenuItem>
-                  <MenuItem onClick={logout}>Sair</MenuItem>
-                </Menu>
-              </div>
-            </div>
-          ) : (
-            <div style={{ display: 'flex', justifyContent: 'flex-end', width: '30%' }}>
-              <Button
-                variant="contained"
-                color="default"
-                className={classes.button}
-                startIcon={<AccountCircle style={{ color: 'rgb(44, 56, 126)' }} />}
-                onClick={login}
-                style={{ background: '#fff' }}
-              >
-                <span style={{ color: 'rgba(0, 0, 0, 0.54)' }}>Entrar</span>
-              </Button>
-            </div>
-          )}
         </Toolbar>
-
       </AppBar>
     </HideOnScroll>
   );
 
-  return renderHeader();
+  function containerModal() {
+    return (
+      <Grid container>
+        <Grid item xs={12} md={12} lg={12}>
+          <textarea
+            id="test"
+          />
+        </Grid>
+        <Grid item xs={12} md={12} lg={12}>
+          <textarea
+            id="test"
+          />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  return (
+    <>
+      {renderHeader()}
+      {renderHeader() && (
+        <Modal
+          container={containerModal()}
+          title="Pedir ajuda"
+          onClick={handleClose}
+          open={open}
+          onClose={handleClose}
+        />
+      )}
+    </>
+  );
 }
 
 export default Header;
